7f2d2c6aca3b14278354bcd059043f3909935980
ridhadanjanny <ridhadanjanny.mail@gmail.com>
2023-05-20 06:22:33 +0700

    get product data fetch_product_cubit.dart

--- lib/module/di/injection.config.dart
+++ lib/module/di/injection.config.dart
@@ -12,28 +12,31 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:paspos/module/data/data_sources/i_product_data_source.dart'
-    as _i6;
-import 'package:paspos/module/data/data_sources/i_user_data_source.dart' as _i9;
-import 'package:paspos/module/data/data_sources/product_data_source.dart'
     as _i7;
-import 'package:paspos/module/data/data_sources/user_data_source.dart' as _i10;
-import 'package:paspos/module/data/repositories/product_repository.dart' as _i8;
+import 'package:paspos/module/data/data_sources/i_user_data_source.dart'
+    as _i10;
+import 'package:paspos/module/data/data_sources/product_data_source.dart'
+    as _i8;
+import 'package:paspos/module/data/data_sources/user_data_source.dart' as _i11;
+import 'package:paspos/module/data/repositories/product_repository.dart' as _i9;
 import 'package:paspos/module/data/repositories/user_new_repository.dart'
-    as _i14;
-import 'package:paspos/module/domain/repositories/i_product_repository.dart'
-    as _i5;
-import 'package:paspos/module/domain/repositories/i_user_repository.dart'
-    as _i13;
-import 'package:paspos/module/domain/use_cases/get_product_use_case.dart'
-    as _i4;
-import 'package:paspos/module/domain/use_cases/i_get_product_use_case.dart'
-    as _i3;
-import 'package:paspos/module/domain/use_cases/i_user_login_use_case.dart'
-    as _i11;
-import 'package:paspos/module/domain/use_cases/user_login_form_use_case.dart'
-    as _i12;
-import 'package:paspos/module/presentation/manager/login_cubit/login_cubit.dart'
     as _i15;
+import 'package:paspos/module/domain/repositories/i_product_repository.dart'
+    as _i6;
+import 'package:paspos/module/domain/repositories/i_user_repository.dart'
+    as _i14;
+import 'package:paspos/module/domain/use_cases/get_product_use_case.dart'
+    as _i5;
+import 'package:paspos/module/domain/use_cases/i_get_product_use_case.dart'
+    as _i4;
+import 'package:paspos/module/domain/use_cases/i_user_login_use_case.dart'
+    as _i12;
+import 'package:paspos/module/domain/use_cases/user_login_form_use_case.dart'
+    as _i13;
+import 'package:paspos/module/presentation/manager/fetch_product_cubit/fetch_product_cubit.dart'
+    as _i3;
+import 'package:paspos/module/presentation/manager/login_cubit/login_cubit.dart'
+    as _i16;
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -46,18 +49,20 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i3.IGetProductUseCase>(
-        () => _i4.GetProductUseCase(gh<_i5.IProductRepository>()));
-    gh.factory<_i6.IProductDataSource>(() => _i7.ProductDataSource());
-    gh.factory<_i5.IProductRepository>(
-        () => _i8.ProductRepository(gh<_i6.IProductDataSource>()));
-    gh.factory<_i9.IUserDataSource>(() => _i10.UserDataSource());
-    gh.factory<_i11.IUserLoginUseCase>(
-        () => _i12.UserLoginFormUseCase(gh<_i13.IUserRepository>()));
-    gh.factory<_i13.IUserRepository>(
-        () => _i14.UserNewRepository(gh<_i9.IUserDataSource>()));
-    gh.factory<_i15.LoginCubit>(
-        () => _i15.LoginCubit(gh<_i11.IUserLoginUseCase>()));
+    gh.factory<_i3.FetchProductCubit>(
+        () => _i3.FetchProductCubit(gh<_i4.IGetProductUseCase>()));
+    gh.factory<_i4.IGetProductUseCase>(
+        () => _i5.GetProductUseCase(gh<_i6.IProductRepository>()));
+    gh.factory<_i7.IProductDataSource>(() => _i8.ProductDataSource());
+    gh.factory<_i6.IProductRepository>(
+        () => _i9.ProductRepository(gh<_i7.IProductDataSource>()));
+    gh.factory<_i10.IUserDataSource>(() => _i11.UserDataSource());
+    gh.factory<_i12.IUserLoginUseCase>(
+        () => _i13.UserLoginFormUseCase(gh<_i14.IUserRepository>()));
+    gh.factory<_i14.IUserRepository>(
+        () => _i15.UserNewRepository(gh<_i10.IUserDataSource>()));
+    gh.factory<_i16.LoginCubit>(
+        () => _i16.LoginCubit(gh<_i12.IUserLoginUseCase>()));
     return this;
   }
 }
